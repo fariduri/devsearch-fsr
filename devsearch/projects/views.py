878c41f8e007232d16fef05f983aadeb1b8399c4
fariduri <fadriduck@gmail.com>
2023-07-26 13:58:14 +0700

    Added dummy data to view

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
 # Create your views here.
 def projects(request):
     return render(request, 'projects/projects.html')
