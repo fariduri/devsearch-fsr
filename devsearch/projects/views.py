e14660e62768d93f896423c08ff1dcb4e9e4aa8f
fariduri <fadriduck@gmail.com>
2023-07-26 13:56:29 +0700

    Setup templates in projects app

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def projects(request):
-    return HttpResponse("These are all our projects")
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse("Project: " + pk)
+    return render(request, 'projects/single-project.html')
